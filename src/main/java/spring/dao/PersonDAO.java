7fde78b963991dbe9e69ec7cecf6d458768b94bf
01egas <38017925+01egas@users.noreply.github.com>
2023-11-23 22:55:39 +0300

    check valid

--- src/main/java/spring/dao/PersonDAO.java
+++ src/main/java/spring/dao/PersonDAO.java
@@ -13,10 +13,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "name1"));
-        people.add(new Person(++PEOPLE_COUNT, "name2"));
-        people.add(new Person(++PEOPLE_COUNT, "name3"));
-        people.add(new Person(++PEOPLE_COUNT, "name4"));
+        people.add(new Person(++PEOPLE_COUNT, "name1", 24, "name1@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "name2", 25, "name2@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "name3", 26, "name3@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "name4", 27, "name4@mail.ru"));
     }
 
     public List<Person> index() {
@@ -35,6 +35,8 @@ public class PersonDAO {
     public void update(int id, Person updatedPerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
--- src/main/java/spring/models/Person.java
+++ src/main/java/spring/models/Person.java
@@ -1,13 +1,44 @@
 package spring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
+    @NotEmpty(message = "Name should not be empty!")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters!")
     private String name;
     private int id;
 
+    @Min(value = 0, message = "Age should be greater than zero!")
+    private int age;
 
-    public Person(int id, String name) {
+    @NotEmpty(message = "email should not be empty!")
+    @Email(message = "email should be valid")
+    private String email;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Person(int id, String name, int age, String email) {
         this.name = name;
         this.id = id;
+        this.age = age;
+        this.email = email;
     }
 
     public Person() {
